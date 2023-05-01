78086ab2b3fa680c6533c5ac6191d8ecf74b3bf3
Atanas Atanasov <94196097+ata-nas@users.noreply.github.com>
2023-05-01 14:00:29 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # medicalrecord-microservices
+
 A microservices version of the record-rest-api project @ https://github.com/ata-nas/record-rest-api
---
+++ doctor-service/src/test/java/com/medicalrecord/doctorservice/DoctorServiceTests.java
@@ -0,0 +1,13 @@
+package com.medicalrecord.doctorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
